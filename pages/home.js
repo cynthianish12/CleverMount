2e3e2f71a88e8f50291fefe43cab2db46ebc09ea
cynthianish12 <cynthianish12@gmail.com>
2024-07-22 23:26:18 +0200

    first commit

--- pages/home.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var myCarousel = document.getElementById('carouselExampleCaptions');
-    var carousel = new bootstrap.Carousel(myCarousel, {
-        interval: 2000 // Change slide every 3 seconds
-    });
-});
